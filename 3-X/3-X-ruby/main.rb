12341723377e1f8f42efe8ec2a8fcf239d9a4773
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-11-18 17:20:49 -0500

    init commit

---
+++ 3-X/3-X-ruby/main.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Program to reverse a string using recursion.
+# @version 1.0
+# @since   2024-11-18
+
+# Recursively reverses a string.
+# @param string_to_reverse [String] The string to be reversed.
+# @return [String] The reversed string.
+def reverse_string(string_to_reverse)
+    if string_to_reverse.empty?
+        return ''
+    end
+
+    first_character = string_to_reverse[0]
+    rest_of_string = string_to_reverse[1..]
+
+    # Reverse the rest of the string and append the first character.
+    reverse_string(rest_of_string) + first_character
+end
+
+# Main functionality of the program.
+def main
+    a_string = 'recursion'
+
+    puts "\nThe original string is: #{a_string}"
+    reversed_string = reverse_string(a_string)
+    puts "The reversed string is: #{reversed_string}"
+
+    puts "\nDone."
+end
+
+main
